cb70635bf5a274de5cf18315f91ea6395caac3bd
Shaqs0 <hhgfhhhggb@gmail.com>
2024-05-24 01:31:35 +0300

    update StartPage

--- src/layout/Top/LayoutTop.tsx
+++ src/layout/Top/LayoutTop.tsx
@@ -4,6 +4,14 @@ export function LayoutTop() {
 
 	return <div className={styles['top']}>
 		<p className={styles['name']}>Dive Destinations</p>
+		<div className={styles['other']}>
+			<ul>
+				<li><a href='#'>About Us</a></li>
+				<li><a href='#'>Dive Spots</a></li>
+				<li><a href='#'>Dive packages</a></li>
+				<li><a href='#'>Contact</a></li>
+			</ul>
+		</div>
 	</div>;
 }
 
--- src/pages/StartPage/StartPage.tsx
+++ src/pages/StartPage/StartPage.tsx
@@ -1,16 +1,51 @@
 import styles from './StartPage.module.css';
 
 export function StartPage() {
-
 	return (
-		<div>
-			<picture className={styles['pic']}>
-				<source
-					srcSet='public/big-480.jpg 480w, public/big-960.jpg 960w, public/big-1200.jpg 1200w, public/big-1920.jpg 1920w'
-					sizes='(max-width: 600px) 480px, (max-width: 1200px) 960px, 100vw'
-				/>
-				<img src='public/big-1200.jpg' alt='Responsive Image'/>
-			</picture>
+		<div className={styles['container']}>
+			<div className={styles['imageContainer']}>
+				<picture className={styles['pic']}>
+					<source
+						srcSet='public/big-1920.jpg 1920w'
+						sizes='100vw'
+					/>
+					<img src='public/big-1920.jpg' alt='Responsive Image' />
+				</picture>
+				<div className={styles['textOverlay']}>Dive into a world of wonder and explore the breathtaking beauty beneath the waves with Dive Destinations</div>
+			</div>
+			<div className={styles['destinations']}>
+				<p className={styles['top_text']}>Discover Our Top Destinations:</p>
+				<div>
+					<p className={styles['text']}>The Great Barrier Reef: Dive into the world’s largest coral reef system, teeming with colorful marine life and stunning coral formations.</p>
+					<picture className={styles['img']}>
+						<source
+							srcSet='public/riff.png 1920w'
+							sizes='100vw'
+						/>
+						<img src='public/riff.png' alt='Responsive Image' />
+					</picture>
+				</div>
+				<div>
+					<p className={styles['text']}>The Blue Hole, Belize: Experience the thrill of diving into this iconic sinkhole, known for its deep blue waters and unique geological formations.</p>
+					<picture className={styles['img']}>
+						<source
+							srcSet='public/blue_holl_1200px.png 1920w'
+							sizes='100vw'
+						/>
+						<img src='public/blue_holl_1200px.png' alt='Responsive Image' />
+					</picture>
+				</div>
+				<div>
+					<p className={styles['text']}>Maldives Atolls: Explore crystal-clear waters, vibrant coral reefs, and an abundance of marine species in this tropical paradise.</p>
+					<picture className={styles['img']}>
+						<source
+							srcSet='public/Maldives.png 1920w'
+							sizes='100vw'
+						/>
+						<img src='public/Maldives.png' alt='Responsive Image' />
+					</picture>
+				</div>
+			</div>
 		</div>
 	);
 }
